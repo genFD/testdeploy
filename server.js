23034b09435fcf8b632b62f87306b7abde975e8f
hermkan <hermann.kanga.dev@gmail.com>
2022-07-16 09:07:41 -0400

    updateddededdsde

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const colors = require('colors');
+// const colors = require('colors');
 const cors = require('cors');
 const serve = require('serve-static');
 const path = require('path');
@@ -10,9 +10,9 @@ const devjobsRoutes = require('./routes/devjobs.routes');
 const app = express();
 app.use(cors());
 
-colors.setTheme({
-  success: ['bgGreen', 'black', 'underline', 'bold'],
-});
+// colors.setTheme({
+//   success: ['bgGreen', 'black', 'underline', 'bold'],
+// });
 
 app.use(express.json());
 
@@ -24,4 +24,4 @@ dotenv.config();
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, console.log(`SERVER IS LISTENING ON PORT ${PORT}`.success));
+app.listen(PORT, console.log(`SERVER IS LISTENING ON PORT ${PORT}`));
